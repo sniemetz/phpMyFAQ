d5327bab79df85cd19fefdccc0b6a39ecbb124a4
Sven Niemetz <sniemetz@gmail.com>
2024-08-10 01:00:34 +0200

    add export for theme style sheet

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     backend: './phpmyfaq/admin/assets/src/index.js',
     frontend: './phpmyfaq/assets/src/frontend.js',
     setup: './phpmyfaq/assets/src/setup.js',
+    svenstyles: './phpmyfaq/assets/themes/sven/scss/style.scss',
     styles: './phpmyfaq/assets/themes/default/scss/style.scss',
     admin: './phpmyfaq/admin/assets/scss/style.scss',
   },
