331e49c4a51df62dd74eb4a5f52be0742a1ce396
Sven Niemetz <sniemetz@gmail.com>
2024-08-10 22:59:09 +0200

    tweaks to expandasearch: make less tall, close if no value

--- phpmyfaq/assets/src/themesjs/sven/custom.js
+++ phpmyfaq/assets/src/themesjs/sven/custom.js
@@ -18,48 +18,74 @@
 
 export const expandSearchForm = () => {
     /// add classes instead of setting styles
+    const doReset = function () {
+        document.querySelectorAll("ul.nav").forEach((o) => {
+            o.style.visibility = "initial";
+        });
+        document.querySelectorAll("div.search form i").forEach((o) => {
+            o.style.visibility = "hidden";
+        });
+        document.querySelector('form#search .searchbar-submit').blur();
+        document.querySelector('form#search .searchclosed').blur();
 
+        document.querySelectorAll("div.topmenu").forEach((o) => {
+            o.style.visibility = "visible";
+        });
+        document.querySelectorAll("div.searchcontainer, div.search").forEach((o) => {
+            o.style.width = "";
+        });
+    
+        document.removeEventListener('keydown', reset);
+        document.removeEventListener('click', reset);
+
+    }
     const reset = function (e) {
         var a = (e.type == "mousedown" &&  e.target.localName != "button"&& e.target.localName != "input");
         var b = (e.type == "keydown" && e.code == "Escape");
         if (a||b) { 
-
-            document.querySelectorAll("ul.nav").forEach((o) => {
-                o.style.visibility = "initial";
-            });
-            document.querySelectorAll("div.search form i, button.searchbar-submit").forEach((o) => {
-                o.style.visibility = "hidden";
-            });
-            document.querySelector('button').blur();
-            document.querySelector('input').blur();
-            document.querySelectorAll("div.topmenu,form#search button.searchclosed").forEach((o) => {
-                o.style.visibility = "visible";
-            });
-            document.querySelectorAll("div.searchcontainer, div.search").forEach((o) => {
-                o.style.width = "";
-            });
-        
-            document.removeEventListener('keydown', reset);
-            document.removeEventListener('click', reset);
+            doReset();
         }
+    }
+    const chk = function (e) {
+        if (e.target.value != "") {
+            document.querySelector('form#search button.searchbar-submit').style.visibility = "visible";
+            document.querySelector('form#search button.searchclosed').style.visibility = "hidden";
+        } else {
+            document.querySelector('form#search button.searchclosed').style.visibility = "visible";
+            document.querySelector('form#search button.searchbar-submit').style.visibility = "hidden";
+        }
+
     }
 
     const expand = function (e) {
-        console.log(e)
+        e.stopPropagation();
+        if ( document.querySelector("div.topmenu").style.visibility == 'hidden' ) {
+            console.log("top menu hidden")
+            if ( document.querySelector('form#search input.searchbar-input').value == "" ) {
+                console.log("no vale - reset")
+                doReset();
+            }
+            return;
+        }
+        console.log("top menu visible ")
+        doExpand();
+    }
 
-        document.querySelectorAll("div.topmenu,form#search button.searchclosed").forEach((o) => {
+    const doExpand = function () {
+        document.querySelectorAll("div.topmenu").forEach((o) => {
             o.style.visibility = "hidden";
         });
         document.querySelectorAll("div.searchcontainer, div.search").forEach((o) => {
             o.style.width = "100%";
         });
     
-        document.querySelectorAll("div.search form i, button.searchbar-submit").forEach((o) => {
+        document.querySelectorAll("div.search form").forEach((o) => {
             o.style.visibility = "visible";
         });
 
         
-        e.stopPropagation();
+        
+        document.querySelector('form#search input.searchbar-input').addEventListener('keyup', chk);
     
         document.addEventListener('keydown', reset);
         document.addEventListener('mousedown', reset);
